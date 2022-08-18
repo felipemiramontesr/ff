0ae1c63addf23e0f82f48f2310533025b134009e
felipemiramontesr <felipemiramontesr@gmail.com>
2022-08-18 14:43:21 -0500

    Modificación de estructura de archivos

--- js/paginaExperienciaMenusNeonEffect.js
+++ js/paginaExperienciaMenusNeonEffect.js
@@ -1,13 +1,14 @@
-/**************************************************************************
-Efecto de neón (box-shadow) según la dirección del scroll en los menús ****
-header y footer de la pagína 'experiencia' '/nodo/25' en su versión movil *  
-**************************************************************************/
+/****************************************************************************************************
+Efecto de neón (box-shadow) según la dirección del scroll en los menús ******************************
+header y footer de la pagína 'experiencia' '/nodo/25' en su versión movil ***************************
+ff**************************************************************************************************/
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
     //Inicialización en 0 de la variable que guarda el ultimo valor de la posición 'Y' del scroll
     let lastScrollTop = 0;
+    window.scrollTo(0, 1);
 
     //Se inicializa la escucha del scroll dentro de la página  
     window.addEventListener("scroll", function(){
@@ -18,53 +19,69 @@ $( document ).ready(function() {
         // => 'st' > 'lastScrollTop' :  cambiar la clase al elemento DOM (on/off)
         if (st > lastScrollTop){
 
-            //Si 'st' contiene algún valor | 'st > lastScrollTop' .           
-            //Neón naranja: on (JQuery)
-            //$("#menu_header_experiencia").removeClass("menu_header_experiencia")
-            //$("#menu_header_experiencia").addClass("menu_header_experiencia_naranja");
+            //Si 'st' contiene algún valor | 'st > lastScrollTop'
+            
+            /*
+            Neón naranja: on (JQuery)
+            $("#menu_header_experiencia").removeClass("menu_header_experiencia")
+            $("#menu_header_experiencia").addClass("menu_header_experiencia_naranja");
+            */
 
-            //Código optimizado
-            //Neón naranja: on (JQuery)
+            /*
+            Código optimizado (JQuery )
+            Neón naranja: on
+            */
             $("#menu_header_experiencia").removeClass("menu_header_experiencia").addClass("menu_header_experiencia_naranja");
 
-            //Neón azul: off (JQuery)
-            //$("#menu_footer_experiencia").removeClass("menu_footer_experiencia_azul")
-            //$("#menu_footer_experiencia").addClass("menu_footer_experiencia");
-
-             //Código optimizado
-             //Neón azul: off (JQuery)
-             $("#menu_footer_experiencia").removeClass("menu_footer_experiencia_azul").addClass("menu_footer_experiencia");
-
-            //Neón azul: off (Vanilla)
             /*
+            Neón azul: off (JQuery)
+            $("#menu_footer_experiencia").removeClass("menu_footer_experiencia_azul")
+            $("#menu_footer_experiencia").addClass("menu_footer_experiencia");
+            */
+
+            /*
+            Código optimizado (JQuery)
+            Neón azul: off 
+            */
+            $("#menu_footer_experiencia").removeClass("menu_footer_experiencia_azul").addClass("menu_footer_experiencia");
+
+            /*
+            Neón azul: off (Vanilla)
             let footer = document.getElementById('menu_footer_experiencia');
             footer.className ='menu_footer_experiencia';
             */
             
-            //Neón naranja: on (Vanilla)
             /*
+            Neón naranja: on (Vanilla)
             let header = document.getElementById('menu_header_experiencia');
             header.className ='menu_header_experiencia_naranja';
             */
 
         // => 'st' != 'lastScrollTop' : cambiar clase al elemento DOM (on/off)
         }else{
-                    
-            //Neón azul: on (JQuery)
-            // $("#menu_footer_experiencia").removeClass("menu_footer_experiencia");
-            // $("#menu_footer_experiencia").addClass("menu_footer_experiencia_azul");
 
-            //Código optimizado
-            //Neón azul: on (JQuery)
+            /*       
+            Neón azul: on (JQuery)
+            $("#menu_footer_experiencia").removeClass("menu_footer_experiencia");
+            $("#menu_footer_experiencia").addClass("menu_footer_experiencia_azul");
+            */
+
+            /*
+            Código optimizado
+            Neón azul: on (JQuery)
+            */
             $("#menu_footer_experiencia").removeClass("menu_footer_experiencia").addClass("menu_footer_experiencia_azul");
 
+            /*
+            Neón naranja: off (JQuery)
+            $("#menu_header_experiencia").removeClass("menu_header_experiencia_naranja");
+            $("#menu_header_experiencia").addClass("menu_header_experiencia");
+            */
 
-            //Neón naranja: off (JQuery)
-            //$("#menu_header_experiencia").removeClass("menu_header_experiencia_naranja");
-            //$("#menu_header_experiencia").addClass("menu_header_experiencia");
-            
-            //Código optimizado
-             //Neón naranja: off (JQuery)
+            /*
+            Código optimizado
+            Neón naranja: off (JQuery)
+            */
             $("#menu_header_experiencia").removeClass("menu_header_experiencia_naranja").addClass("menu_header_experiencia")
 
             //Neón azul: on (Vanilla)
@@ -72,7 +89,7 @@ $( document ).ready(function() {
             let footer = document.getElementById('menu_footer_experiencia');
             footer.className ='menu_footer_experiencia_azul';
             */
-
+            
             //Neón naranja: off (Vanilla)
             /*
             let footer = document.getElementById('menu_header_experiencia');
@@ -82,6 +99,7 @@ $( document ).ready(function() {
         /*Se asigna el valor actual de la posición 'Y' del scroll (st) a la variable que 
         guarda el ultimo valor de la posición 'Y' del scroll (lastScrollTop)*/
         lastScrollTop = st;
-
+    
     }, false);
+
 });
